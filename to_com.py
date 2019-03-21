1b3148a7f7b748704021287aacc30096fc2568a4
root <root@AdjMat.lw5lw24temde3cto0qbrpdjmih.fx.internal.cloudapp.net>
2019-03-21 11:37:11 +0000

    working on the common link algorithm

--- to_com.py
+++ to_com.py
@@ -1,46 +1,65 @@
 import pymongo as pm
-import threading
+import multiprocessing
 
 client = pm.MongoClient()
 
 db = client.adj_mat
 
+print("Clearing collection")
+db.common_adj.delete_many({})
+
 def get_common(x, y):
 
     x_set = set(x)
-    y_query = db.first_adj.find({"subject:'%s'", y})
-    y_set = set(y_query.neighbours)
+    filter = {"subject": str(y.replace(".",""))}
+    try:
+        
+        y_query = db.first_adj.find_one(filter)
+        y_set = set(y_query["neighbours"])
 
-    common = x_set.intersection(y_set)
+        common = x_set.intersection(y_set)
 
-    return len(common)
+        return len(common)
 
-def process_cursor(cursor):
+    except Exception as e:
+        #print("couldn't find", filter)
+        #print(e)
+        return 0
+
+def process_cursor(skip_n, limit_n):
 
     print("cursor spinning up")
 
-    for i, document in enumerate(db.first_adj.find()):
-
-        subject = document.subject
-        neighbours = document.neighbours
-
+    for i, document in enumerate(db.first_adj.find().skip(skip_n).limit(limit_n)):
+        try:
+            subject = document["subject"]
+            neighbours = document["neighbours"]
+        except Exception as e:
+            #print(e)
+            #print(document)
+            continue
+            
         document = {subject: dict()}
 
         for j in neighbours:
 
             common = get_common(neighbours, j)
-            document[subject][j] = common
+            document[subject.replace(".", "")][j.replace(".","")] = common
 
-        db.first_adj.insert_one(document)
+        #print(document)
+        db.common_adj.insert_one(document)
 
         if i % 10000 == 0:
 
             print("Documents processed:", i)
+            
 
 
-cursors = db.first_adj.parallel_scan(7)
-
-threads = [threading.Thread(target=process_cursor, args=(cursor,))for cursor in cursors]
+cores = 7
+collection_size = db.first_adj.count()
+batch_size = collection_size//7
+skips = range(0, collection_size, batch_size)
+threads = [multiprocessing.Process(target=process_cursor, args=(skip_n, batch_size))for skip_n in skips]
 
 for thread in threads:
     thread.start()
