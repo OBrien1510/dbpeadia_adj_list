93d5714713d04334d9337cf0adf134dfce261955
OBrien1510 <hug_hob@hotmail.com>
2019-05-20 08:42:18 +0100

    fixing bugs in linked list class

--- LinkedList.py
+++ LinkedList.py
@@ -34,6 +34,14 @@ class Node:
 
 class LinkedList:
 
+    """
+    Class to implement a linked list for semantic similarity
+    linked list is aranged in descending order of similarity
+    head represents highest sim and tail represents lowest sim
+    note: the higher the sim, the lower the distance value and vice versa
+    (so head actually has the lowest numerical value)
+    """
+
     def __init__(self, length=50):
         self._head = None
         self._tail = None
@@ -56,6 +64,9 @@ class LinkedList:
         self.linkedlist[n.subject] = n.node
         self._head = n
         self.length += 1
+        if self.length > self._max_length:
+            self.remove_tail()
+            self.length -= 1
 
         if self.length > self._max_length:
             self.remove_tail()
@@ -63,27 +74,46 @@ class LinkedList:
 
         self.linkedlist["length"] = self.length
 
-    def add_node_middle(self, n):
+    def add_node_middle(self, n, update):
 
         current = self._head.get_next()
         previous = self._head
 
         while current is not None and current.get_next() is not None:
 
+            # if the the sim is less than the current node but greater than the previous node
+            # then we want to insert between the two
             if (n.get_sim() <= current.get_sim()) and (n.get_sim() >= previous.get_sim()):
                 n.change_next(current)
                 previous.change_next(n)
                 self.linkedlist[n.subject] = n.node
                 self.length += 1
                 self.linkedlist["length"] = self.length
-                break
+                # if we are updating a node's similarity we want to continue on until we find the old node to delete
+                if update:
+                    previous = current
+                    current = current.get_next()
+                else:
+                    break
+            # if we are continuing after an update and we find the old node, delete it
+            elif update and current.get_next().subject == n.subject:
+                # remove old node
+                self.remove_middle(current, current.get_next)
 
             else:
 
                 previous = current
                 current = current.get_next()
 
+    def remove_middle(self, previous, current):
+
+        nextval = current.get_next()
+        previous.change_next(nextval)
+        del nextval
+
     def remove_tail(self):
+        # sometimes adding a new node takes it over the max length
+        # in this case simply remove the tail to return to max length
 
         current = self._head
 
@@ -109,7 +139,14 @@ class LinkedList:
 
         if node is not None:
 
-            node.set_sim(n.get_sim()**2)
+            # reduce not distance metric by raising it to the power of 2
+            new_distance = node.get_sim()**2
+
+            # create new node with new similiarity
+            new_node = Node(new_distance, node.get_next(), node.subject)
+
+            # insert the new node while deleting the old one
+            self.add_node_middle(new_node, True)
 
     def search(self, n):
 
@@ -135,30 +172,19 @@ class LinkedList:
             self._tail = n
             return
 
-        exists = self.search(n.subject)
-
-        if exists is not None:
-
-            if exists.get_sim() < n.get_sim():
-
-                exists.set_sim(n.get_sim()**2)
-
-            else:
-
-                exists.set_sim(exists.get_sim()**2)
-
-            return
+        self.update_node(n)
 
         if n.get_sim() <= self._head.get_sim():
 
             self.add_node_head(n)
 
+        # no case where we ever want to add to tail if list is already at max length
         elif n.get_sim() >= self._tail.get_sim() and self.length < 50:
 
             self.add_node_tail(n)
 
         elif (n.get_sim() > self._head.get_sim()) and (n.get_sim() < self._tail.get_sim() and self.length < 50) or (self.length < 50):
-            self.add_node_middle(n)
+            self.add_node_middle(n, False)
 
     def get_head(self):
 
